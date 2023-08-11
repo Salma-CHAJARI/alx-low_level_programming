35bcf861a4e9d584c5bce6a5b49ab8b29f847fb6
Salma-CHAJARI <yaqout02@gmail@gmail.com>
2023-08-10 18:17:49 -0700

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - entry poit
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+      puts("\"Programming is like building a multilingual puzzle");
+      return (0);
+}
