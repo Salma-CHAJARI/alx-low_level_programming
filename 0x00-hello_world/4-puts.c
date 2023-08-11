a9bc570bd9b34d7f3d09dbe1fad7c10a8e591d58
Salma-CHAJARI <yaqout02@gmail@gmail.com>
2023-08-10 18:22:46 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * main - entry poit
- * Return: always 0 (success)
+ *main - entry poit
+ *Return: always 0
 */
+
 int main(void)
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
