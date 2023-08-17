ae03013957b112db314781a8e3f9d86f3f444ab2
Salma-CHAJARI <yaqout02@gmail@gmail.com>
2023-08-17 12:24:50 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,13 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
-			_putchar(j + '0');
+		{
+			if (j > 9)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
+		}
 		_putchar('\n');
 	}
 }
