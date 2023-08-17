3ef2bd75f971ec264605abb6afe44a2db7a8d56e
Salma-CHAJARI <yaqout02@gmail@gmail.com>
2023-08-17 16:51:05 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,20 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0 && (i % 5) != 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf("FizzBuzz ");
 		}
-		else if ((i % 5) == 0 && (i % 3) != 0)
+		else if (i % 3 == 0)
 		{
-			printf("Buzz");
-			printf(" ");
-		}
-		else if ((i % 5) == 0 && (i % 3) == 0)
+			printf("Fizz ");
+		} else if (i % 5 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", i);
-			printf(" ");
+			printf("%d ", i);
 		}
 	}
 	printf("\n");
