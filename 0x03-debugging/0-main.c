51175d429677d144dcc3751ca76077333a3af180
Salma-CHAJARI <yaqout02@gmail@gmail.com>
2023-08-16 18:05:07 -0700

    fixed

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test fonction positive_or_negative
+ * Returm: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - infinite loop
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :<\n");
+
+	i = 0;
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+	return (0);
+}
