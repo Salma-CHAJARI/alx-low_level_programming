77dc580526b2037cc59cd539e7ef0a8270ef0a4d
Salma-CHAJARI <yaqout02@gmail@gmail.com>
2023-08-24 17:27:49 -0700

    advenced tasks

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string in rot13
+ * @s: string
+ * Return: the resulting string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (s[j] == a[i])
+			{
+				s[j] = b[i];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
